89a5496d05a1043a99a95f421cbf31ef60dfab36
eXclusive Tshirt <116873249+TabibeeAlvarez@users.noreply.github.com>
2024-02-01 03:23:57 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ function starts(){
     window.location.href = 'start.html'
 }
 
+document.getElementById('button-option').style.display = 'none';
+
 function option(){
-    alert('This function is not available right now! because youre not my option <3')
+    document.getElementById('button-option').style.display = 'block';
+    document.getElementById('body1').style.display = 'none';
+}
+
+function backs(){
+    document.getElementById('button-option').style.display = 'none';
+    document.getElementById('body1').style.display = 'block';
 }
